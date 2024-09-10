fc6baf073e3673a5c036bb9b338eb3fee927e36f
Umar <mrumar628@gmail.com>
2024-09-10 20:27:54 +0200

    main

---
+++ Chapter_1/1_Hello_World.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /* "#include" is a reserved term to import code, in this case to import standard input/output library */
+
+main(){     /* "main" another reserved term, the compiler starts from main, the start point of my program */
+    printf("Hello World\n"); /* printf = print format */
+}
+
+/* to exec "gcc 1_Hello_World.c -o 1_Hello_World"  */
